30a39039200f3a1de511f2b4c1bd911e6568c32e
NoahSolis <NSolis1993@gmail.com>
2022-04-04 10:11:54 -0500

    Begin work on incremental increase. Button adds by one to console.

---
+++ app.js
@@ -0,0 +1,15 @@
+// initialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the HTML
+
+// TODO: BE SURE TO INSTALL QUOKKA AT WORK
+
+let count = 0
+
+function increment() {
+    count = count + 1
+    console.log(count)
+}
+
+increment()
