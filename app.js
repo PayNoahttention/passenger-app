e42d566116f8b917f3095459cf167b5cd3c900f2
Noah Solis <NSolis1993@gmail.com>
2022-04-04 16:04:08 -0500

    Increment button working and adding to the count. Added save button.

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-// initialize the count as 0
-// listen for clicks on the increment button
-// increment the count variable when the button is clicked
-// change the count-el in the HTML
-
-// TODO: BE SURE TO INSTALL QUOKKA AT WORK
-
-let count = 0
+let countEl = document.getElementById("count-el");
+let count = 0;
 
 function increment() {
     count = count + 1
-    console.log(count)
+    countEl.innerText = count;
 }
 
 increment()
+
+function save() {
+    console.log(count)
+}
+
+save()
